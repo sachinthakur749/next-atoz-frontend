686a5c982072e9c85b9c088d05c5a122f35473b9
sachinthakur749 <sachinthakur9983@gmail.com>
2025-04-23 23:09:02 +0545

    add:locale

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,8 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 export default function Home() {
+  const t = useTranslations();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -24,6 +26,8 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
+
+        {t("homepage")}
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
